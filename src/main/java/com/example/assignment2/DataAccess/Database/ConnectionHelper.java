9b47a8e62b625d1a35161b563c1a86b429004b91
Wessel Konstantinov <wes_kon1@hotmail.com>
2022-02-21 16:02:50 +0100

    Define database connection url

---
+++ src/main/java/com/example/assignment2/DataAccess/Database/ConnectionHelper.java
@@ -0,0 +1,5 @@
+package com.example.assignment2.DataAccess.Database;
+
+public class ConnectionHelper {
+    public static final String CONNECTION_URL = "jdbc:sqlite:src/main/resources/Chinook_Sqlite.sqlite";
+}
