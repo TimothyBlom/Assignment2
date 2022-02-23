90da8eb00178c549681f7ceec2606c0b0dc5dbb5
Wessel Konstantinov <wes_kon1@hotmail.com>
2022-02-23 20:07:59 +0100

    Complete controllers and views (home and search results page)

--- src/main/java/com/example/assignment2/Controllers/HomeController.java
+++ src/main/java/com/example/assignment2/Controllers/HomeController.java
@@ -1,51 +1,24 @@
 package com.example.assignment2.Controllers;
 
-import com.example.assignment2.DataAccess.Models.Artist;
-import com.example.assignment2.DataAccess.Models.Genre;
-import com.example.assignment2.DataAccess.Models.Track;
 import com.example.assignment2.DataAccess.Repositories.ArtistRepository;
 import com.example.assignment2.DataAccess.Repositories.GenreRepository;
 import com.example.assignment2.DataAccess.Repositories.TrackRepository;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.ArrayList;
-import java.util.Collections;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-//@RestController
-@RequestMapping("/songs")
 public class HomeController {
     private final TrackRepository trackRepository = new TrackRepository();
     private final ArtistRepository artistRepository = new ArtistRepository();
     private final GenreRepository genreRepository = new GenreRepository();
 
-    @GetMapping("/tracks")
-    public ArrayList<Track> getTracks() {
-        return trackRepository.getTracks();
-    }
-
-    @GetMapping("/artists")
-    public ArrayList<Artist> getArtist() {
-        Collections.shuffle(artistRepository.getArtists());
-        return artistRepository.getArtists();
-    }
-
-    @GetMapping("/genres")
-    public ArrayList<Genre> getGenres() {
-        return genreRepository.getGenres();
-    }
-
-    @GetMapping("/home")
-    public String homePageView() {
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String homePageView(Model model) {
+        model.addAttribute("randomArtists", artistRepository.getRandomArtists(5));
+        model.addAttribute("randomTracks", trackRepository.getRandomTracks(5));
+        model.addAttribute("randomGenres", genreRepository.getRandomGenres(5));
         return "Home";
     }
-
-    @GetMapping("/search")
-    public String searchPageView() {
-        return "Search";
-    }
-
 }
--- src/main/java/com/example/assignment2/Controllers/SearchController.java
+++ src/main/java/com/example/assignment2/Controllers/SearchController.java
@@ -1,11 +1,19 @@
 package com.example.assignment2.Controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import com.example.assignment2.DataAccess.Repositories.SearchResultRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import java.util.ArrayList;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
+@Controller
 public class SearchController {
+    private final SearchResultRepository searchResultRepository = new SearchResultRepository();
 
-
-
+    @RequestMapping(value = "/search", method = RequestMethod.GET)
+    public String searchPageView(@RequestParam String query, Model model) {
+        model.addAttribute("searchResults", searchResultRepository.getSearchResults(query));
+        return "Search";
+    }
 }
